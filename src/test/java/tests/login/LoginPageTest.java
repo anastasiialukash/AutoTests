b982c3087094e2c94d5210d619d44f8d9c5c537f
Anastasia Lukash <anastasia16rom@gmail.com>
2021-03-06 00:34:37 +0300

    change limit

--- src/test/java/tests/login/LoginPageTest.java
+++ src/test/java/tests/login/LoginPageTest.java
@@ -13,11 +13,10 @@ public class LoginPageTest extends BaseTest {
     protected String NAME = "TestName";
     protected String EMAIL = "test@gmail.com";
     protected String PASSWORD = "12345";
-    protected String LIMIT = "2000";
+    protected String LIMIT = "2500";
 
     @Test
     public void loginAsAdmin() {
-        System.out.println("Login");
         basePage.goToUrl(LINK);
         loginPage.loginAsAdmin();
         loginPage.logout();
